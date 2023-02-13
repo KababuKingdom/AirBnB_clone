ce36465db4528c49f6526c75435d28dec1d1ec8c
kababukingdom <kababukingdom@gmail.com>
2023-02-13 15:14:10 +0000

    Contains the tests for class HBNBCommand

--- tests/test_console.py
+++ tests/test_console.py
@@ -16,6 +16,13 @@ import sys
 import unittest
 from models import storage
 from models.engine.file_storage import FileStorage
+from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.place import Place
+from models.amenity import Amenity
+from models.review import Review
 from console import HBNBCommand
 from io import StringIO
 from unittest.mock import patch
